7859dbba87f05cb38fb3b09b3d28616fb0fc12f9
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2024-12-22 20:19:28 +0900

    Update 평범한 배낭.py

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -21,3 +21,19 @@ dp = [[-1 for _ in range(100_001)] for _ in range(N+1)]
     
 recur(0,0)
 print(dp[0][0])
+
+#=======================================#
+#점화식 bottomUp
+N, B =map(int, input().split())
+
+items = [list(map(int,input().split())) for _ in range(N)]
+#2차원 배열 y 는 무게
+dp = [[0]*(B+1) for _ in range(N+1)]
+    
+for idx in range(1, N+1) :
+    for weight in range(1,B+1) :
+        if weight < items[idx-1][0]: #최대 무게 보다 작으면 그래로 가져온다
+            dp[idx][weight] = dp[idx-1][weight]
+        else : 
+            dp[idx][weight] = max(dp[idx-1][weight - items[idx-1][0]] + items[idx-1][1], dp[idx-1][weight])
+print(dp[N][B])
