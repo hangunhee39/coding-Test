c9f96f1b21316e1b7eea12b7afbf0bd6c6ddceb9
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-01-09 22:30:21 +0900

    Update 최소 스패닝 트리.py

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,3 +1,51 @@
+# MST 최소 스패닝 트리 (minimum spanning tree)
+# 최소한(비용)으로 뻗어나가는 트리
+ 
+# 프림 : 다익스트라 -> 트리(mst)
+# 다익스트라를 이용해서, 방문한 곳인지 확인
+# 1. 방문하면서 링크 정보를 저장 (한 리스)
+# 2. 방문 한 곳은 다시 연결하지 않는다.
+
+# 크루스칼 : 유니온파인드 -> 트리
+# 1.일단 간 정보 전부 가져옴 (정렬해서)
+# 2.find 한 후 같은 집합이 아니면 union
+
+
+# 그래프를 트리(사이클x)로 바꾼다
+
+# 프림 방식
+import heapq
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+visited = [ 0 for _ in range(N+1)]
+
+for _ in range(M) :
+    A, B, C = map(int, input().split())
+
+    graph[A].append([C,B])
+    graph[B].append([C,A])
+
+answer = 0
+
+# 다익스트라 !
+
+q = [[0,1]] # 가중치 0, 1에서 출발
+
+while q :
+    # 최소비요을 꺼냄 (리스트 앞에 있는 거 기준)
+    weight,node = heapq.heappop(q)
+    
+    if visited[node] == 0:
+        visited[node] = 1
+        answer += weight
+
+        for next in graph[node] :
+            heapq.heappush(q, next)
+        
+print(answer)
+#----------------------------------------------------------------------
 
 #크루스칼
 
