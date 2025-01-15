f655c132627c147a06b2bc808647c6def5798d5d
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-01-15 23:48:43 +0900

    Update 유기농 배추.py

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,5 +1,6 @@
 #DFS 방식
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
 
 T = int(input())
@@ -16,13 +17,31 @@ def dfs(y,x):
         if 0<= ey < Y and 0 <= ex < X:
             dfs(ey, ex)
     return
+
+#BFS 방식
+def bfs(y,x) :
+    q = deque()
+    q.append([y,x])
+    graph[y][x] = 0
+
+    while q:
+        ey ,ex = q.popleft()
+        graph[ey][ex] = 0
         
+        for dy, dx in [[1,0],[-1,0],[0,1],[0,-1]]:
+            ny = ey + dy
+            nx = ex + dx
+
+            if 0<= ny < Y and 0 <= nx < X and graph[ny][nx] == 1:
+                graph[ny][nx] = 0
+                q.append([ny,nx])
+            
+    return
 
 for _ in range(T) :
     answer = 0
     X, Y ,N = map(int, input().split())
     graph = [[0 for _ in range(X)] for _ in range(Y)]
-    visited = [[0 for _ in range(X)] for _ in range(Y)]
     
     for _ in range(N):
         x, y = map(int, input().split())
@@ -32,7 +51,8 @@ for _ in range(T) :
         for x in range(X):
             if graph[y][x] == 1:
                 answer += 1
-                dfs(y,x)
+                #dfs(y,x)
+                bfs(y,x)
 
     print(answer)
             
