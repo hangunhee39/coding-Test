f7e77f9e2c4f4f24f1e6e9d03b31971a3dd5c326
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-01-28 00:18:15 +0900

    Update 생태학.py

--- 백준/Silver/4358. 생태학/생태학.py
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -20,4 +20,27 @@ arr = sorted(Counter(arr).items()) # 결과를 튜플로 된 리스트
 
 for i in range(len(arr)) :
     print("%s %.4f" %(arr[i][0], arr[i][1]/total * 100))
-        
+
+#2. dict(HashMap) 사용하기
+# * dict는 순서가 보장됨
+
+import sys
+input = sys.stdin.readline
+
+total = 0
+hashMap = dict()
+
+while True:
+    tree = input().rstrip()
+    if not tree:
+        break
+    total += 1
+    if tree in hashMap:
+        hashMap[tree] += 1
+    else :
+        hashMap[tree] = 1
+
+hashMap = dict(sorted(hashMap.items()))
+
+for key, value in hashMap.items() :
+    print("%s %.4f" %(key, value/total * 100))
