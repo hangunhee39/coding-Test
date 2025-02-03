b33dc96ac85694fc3b408078fe0ec70d5f736a5c
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-02-03 22:50:42 +0900

    Update 말이 되고픈 원숭이.py

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,46 +1,35 @@
-from collections import deque
+# BFS 
+from collections import deque 
 
-# 입력값
-k = int(input())
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+K = int(input())
+W, H = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(H)]
 
-# 상하좌우, 체스 나이트 이동
-dist = [[1,0],[0,1],[-1,0],[0,-1]]
-horse = [[-2,-1], [-2,1],[-1,-2],[-1,2],[2,-1],[2,1],[1,-2],[1,2]]
+monkey = [[-1,0],[0,1],[1,0],[0,-1]]
+horse = [[-2,1],[-1,2],[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1]]
 
 def bfs():
-  visited = [[[0]*(k+1) for _ in range(m)] for _ in range(n)]
-  queue = deque()
-  queue.append([0,0,0])
-  visited[0][0][0] = 1
+    # 횟수 저장 -> [Y좌표][X좌표][K개수]
+    count = [[[-1]*(K+1) for _ in range(W)]for _ in range(H)]
+    q = deque()
+    q.append([0,0,K])
+    while q :
+        y, x, k = q.popleft()
+        if y == H-1 and x == W-1:
+            return count[y][x][k]+1
+        if k > 0:
+            for dy, dx in horse:
+                ny = y + dy
+                nx = x + dx
+                if 0 <= ny < H and 0 <= nx < W and graph[ny][nx] == 0 and count[ny][nx][k] == -1:
+                    q.append([ny, nx , k-1])
+                    count[ny][nx][k-1] = count[y][x][k] + 1
 
-  while queue:
-    x, y, z = queue.popleft()
-
-    # 목표 지점에 도달하면 return
-    if x==n-1 and y==m-1:
-      return visited[x][y][z]-1
-
-    # 상하좌우로 이동
-    for (i,j) in dist:
-      dx, dy = x+i, y+j
-      if 0<=dx<n and 0<=dy<m and not visited[dx][dy][z] and not graph[dx][dy]:
-        visited[dx][dy][z] = visited[x][y][z]+1
-        queue.append([dx,dy,z])
-
-    # 말 이동 수보다 작을 경우에만 이동
-    if z<k:
-      for (hi, hj) in horse:
-        hx, hy = x+hi, y+hj
-        if 0<=hx<n and 0<=hy<m:
-          if not graph[hx][hy]:
-            # z+1번째 말처럼 이동하는 중
-            if not visited[hx][hy][z+1]:
-              visited[hx][hy][z+1] = visited[x][y][z]+1
-              queue.append([hx,hy,z+1])
-
-  return -1
-
-# 정답 출력
+        for dy, dx in monkey:
+            ny = y + dy 
+            nx = x + dx
+            if 0 <= ny < H and 0 <= nx < W and graph[ny][nx] == 0 and count[ny][nx][k] == -1:
+                q.append([ny, nx, k])
+                count[ny][nx][k] = count[y][x][k] + 1         
+    return -1
 print(bfs())
