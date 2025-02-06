0ce00712a860f0529f3c90d3cba8ceda83a23052
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-02-06 22:48:01 +0900

    Update 퍼레이드.py

--- 백준/Gold/16168. 퍼레이드/퍼레이드.py
+++ 백준/Gold/16168. 퍼레이드/퍼레이드.py
@@ -1,56 +1,56 @@
+# 유니온 파인드, bfs
+# 오일러 알고리즘 (한붓 긋기)
 import sys
+sys.setrecursionlimit(10**6)
 
-def getParent(x):
-    if parent[x]==x:
-        return x
+def _union(A,B):
+    A = _find(A)
+    B = _find(B)
+
+    if A == B:
+        return
+    if rank[A] < rank[B]:
+        par[A] = B
+    elif rank[B] < rank[A] :
+        par[B] = A
+    else :
+        par[A] = B
+        rank[B] += 1
+
+def _find(A) :
+    if par[A] == A:
+        return A
+    else :
+        par[A] = _find(par[A])
+        return _find(par[A])
+        
     
-    parent[x]=getParent(parent[x])
-    return parent[x]
 
+V, E = map(int, input().split())
+par = [ i for i in range(V+1)] # 부모 확인
+rank = [ 0 for _ in range(V+1)] # 높이 - 유니온 시간 단축용
+edge = [ 0 for _ in range(V+1)] # 간선 
 
-def isSameParent(x,y):
-    return getParent(x)==getParent(y)
+for _ in range(E):
+    A,B = map(int, input().split())
+    _union(A,B)
+    edge[A] += 1
+    edge[B] += 1
 
+# 오일러 조건 
+# 1.간선의 개수가 전부 짝수이거나
+# 2. 딱 2개만 홀수이다
+count = 0
 
-def unionParent(x,y):
-    x=getParent(x)
-    y=getParent(y)
-
-    if x<y:
-        parent[y]=x
-    else:
-        parent[x]=y
-
-
-#메인 코드 부분
-v,e=map(int,sys.stdin.readline().split())
-
-parent=[i for i in range(v+1)]
-
-edge=[[] for _ in range(v+1)]
-for _ in range(e):
-    v1,v2=map(int,sys.stdin.readline().split())
-    edge[v1].append(v2)
-    edge[v2].append(v1)
-    
-    if not isSameParent(v1,v2):
-        unionParent(v1,v2)
-
-
-# 모든 노드가 다같이 연결되었는지 확인 (공통의 부모를 공유하는지 파악)
-base=getParent(1) # 기준: 1번째 노드의 부모로 설정.
-for i in range(2, v+1):
-    if base!=getParent(i):
-        print('NO')
-        exit(0)
-
-odd_cnt=0 # 차수가 홀수인 노드 개수 카운트.
-for i in range(1,v+1):
-    if len(edge[i])%2==1:
-        odd_cnt+=1
-
-
-if odd_cnt==2 or odd_cnt==0:
-    print('YES')
-else:
-    print('NO')
+# 모든 지점이 연결된 그래프이다
+if all(par[1] == par[i] for i in range(1,V+1)):
+    for e in edge:
+        if e%2 == 1:
+            count += 1
+    if count == 2 or count == 0:
+        print("YES")
+    else :
+        print("NO")
+else :
+    print("NO")
+# 97퍼 실패 -> 이유를 모르겠음
