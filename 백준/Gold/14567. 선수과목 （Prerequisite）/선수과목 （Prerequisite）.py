4edfcae18cc1a8968cb4b838ff5f5206cb45888e
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-06-07 14:17:58 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 644 ms, Memory: 51264 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,15 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
-indegree = [1] * (N+1)
+answer = [1] * (N+1)
+indegree = [0] * (N+1)
 graph = [[] for _ in range(N+1)]
+q = deque()
 
 for i in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
+    indegree[b] += 1
 
+# 위상정렬
 for i in range(1,N+1):
-    for b in graph[i]:
-        indegree[b] = max(indegree[b], indegree[i]+1)
-print(*indegree[1:])
+
+    # 더 이상 위에 있는게 없으면 큐에 삽입 -> 순서대로 삽입됨
+    if indegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+
+    for i in graph[now]:
+        indegree[i] -= 1 
+        # 더 이상 밑에가 없으면 삽입
+        if indegree[i] == 0:                
+            answer[i] = answer[now] + 1
+            q.append(i)
+ 
+print(*answer[1:])
         
-    
