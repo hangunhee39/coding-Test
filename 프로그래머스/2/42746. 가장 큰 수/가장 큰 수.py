f60901601d18c4b1fed0bd928073d5db5fe6b334
Han Geonhui <77563098+hangunhee39@users.noreply.github.com>
2025-07-14 23:56:42 +0900

    [level 2] Title: 가장 큰 수, Time: 1326.44 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,3 @@
 def solution(numbers):
-    arr = list(map(str, numbers))
-    # 1000 미만 이여서 3번 곱하면 비교 가능 (문자 형태 일때)
-    # 7 -> 777
-    # 73 -> '737' 373
-    # 730 -> '730' 730730
-    arr.sort(key = lambda x: x*3, reverse = True)
-    answer = str(int(''.join(arr)))
-    
+    answer = ''
     return answer
